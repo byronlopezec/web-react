c6b62e9ef56e92d11a41b4e4da9f1458bf7866cf
Byron Lopez <byron.lopez.begins@gmail.com>
2021-11-08 13:18:32 -0500

    tarea memo and useCallback

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { MemoHook } from './components/06-memos/MemoHook';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
@@ -12,8 +12,8 @@ import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { Layout } from './components/05--useLayoutEffect/Layout';
 // import { Memorize } from './components/06-memos/Memorize';
+import { Padre } from './components/07-tarea-memo/Padre';
 
-
-ReactDOM.render(<CallbackHook />,
+ReactDOM.render(<Padre />,
     document.getElementById('root')
 );
