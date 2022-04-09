eab16c8ff2ba897020af03bb7ef8c78b3f828621
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-08 22:36:41 -0500

    useForm

--- 07-heroes-app/src/components/search/SearchScreen.js
+++ 07-heroes-app/src/components/search/SearchScreen.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useForm } from './../../hooks/useForm';
 
 export const SearchScreen = () => {
+
+    const [values, handleInputChange] = useForm({ searchText: '', });
+
+    const { searchText } = values;
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchText);
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+            <h1> Búsquedas</h1>
+            <hr />
+
+            <div className='row'>
+                <h4>Buscar</h4>
+                <hr />
+
+                <form onSubmit={handleSearch}>
+                    <input
+                        type="text"
+                        placeholder='Buscar Héroe'
+                        className='form-control'
+                        name="searchText"
+                        autoComplete='off'
+                        value={searchText}
+                        onChange={handleInputChange} />
+
+                    <button className='btn btn-outline-primary mt-1'
+                        type='submit'>
+                        Buscar...
+                    </button>
+                </form>
+            </div>
+        </>
     )
 }
--- 07-heroes-app/src/components/ui/Navbar.js
+++ 07-heroes-app/src/components/ui/Navbar.js
@@ -36,6 +36,12 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
+                    <NavLink
+                        className={(isActive) => { return "nav-item nav-link " + (isActive && 'active') }}
+                        to="/search"
+                    >
+                        Search
+                    </NavLink>
                 </div>
             </div>
 
