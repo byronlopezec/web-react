27f0848a0198942b8eea20207822247abb6d58e3
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-08 22:49:27 -0500

    search heroe by name

--- 07-heroes-app/src/components/search/SearchScreen.js
+++ 07-heroes-app/src/components/search/SearchScreen.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useForm } from './../../hooks/useForm';
+import { getHeroesByName } from './../../selectors/getHeroesByName';
+import { HeroCard } from './../hero/HeroCard';
 
 export const SearchScreen = () => {
 
     const [values, handleInputChange] = useForm({ searchText: '', });
 
     const { searchText } = values;
+    const heroesFiltered = getHeroesByName("searchText");
+
 
     const handleSearch = (e) => {
         e.preventDefault();
@@ -17,24 +21,36 @@ export const SearchScreen = () => {
             <hr />
 
             <div className='row'>
-                <h4>Buscar</h4>
-                <hr />
+                <div className='col-5'>
+                    <h4>Buscar</h4>
+                    <hr />
 
-                <form onSubmit={handleSearch}>
-                    <input
-                        type="text"
-                        placeholder='Buscar Héroe'
-                        className='form-control'
-                        name="searchText"
-                        autoComplete='off'
-                        value={searchText}
-                        onChange={handleInputChange} />
+                    <form onSubmit={handleSearch}>
+                        <input
+                            type="text"
+                            placeholder='Buscar Héroe'
+                            className='form-control'
+                            name="searchText"
+                            autoComplete='off'
+                            value={searchText}
+                            onChange={handleInputChange} />
 
-                    <button className='btn btn-outline-primary mt-1'
-                        type='submit'>
-                        Buscar...
-                    </button>
-                </form>
+                        <button className='btn btn-outline-primary mt-1'
+                            type='submit'>
+                            Buscar...
+                        </button>
+                    </form>
+                </div>
+                <div className='col-7'>
+                    <h4>Resultados</h4>
+                    <hr />
+                    {
+                        heroesFiltered.map(hero => (<HeroCard
+                            key={hero.id}
+                            {...hero}
+                        ></HeroCard>))
+                    }
+                </div>
             </div>
         </>
     )
---
+++ 07-heroes-app/src/selectors/getHeroesByName.js
@@ -0,0 +1,11 @@
+
+import { heroes } from './../data/heroes';
+
+
+export const getHeroesByName = (name = "") => {
+
+    // name = name.toLowerCase();
+    // return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
+
+    return heroes;
+}
