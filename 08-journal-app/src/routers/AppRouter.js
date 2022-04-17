39c47afdde9056ab7a69e24dd79d1c2636105bf8
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-17 14:14:57 -0500

    UseEffect onAuthStateChanged

--- 08-journal-app/src/routers/AppRouter.js
+++ 08-journal-app/src/routers/AppRouter.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from './../components/journal/JournalScreen';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        onAuthStateChanged(getAuth(), (user) => {
+
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+            }
+        })
+    }, [dispatch])
     return (
         <BrowserRouter>
             <Routes>
