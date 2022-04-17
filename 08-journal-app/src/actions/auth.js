8fd930f4277abce0d595673ce668587685c3d014
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-17 13:17:29 -0500

    create user with password email

--- 08-journal-app/src/actions/auth.js
+++ 08-journal-app/src/actions/auth.js
@@ -1,6 +1,6 @@
 import { googleAuthProvider } from '../firebase/firebase-config';
 import { types } from './../types/types';
-import { getAuth, signInWithPopup } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, signInWithPopup, updateProfile } from 'firebase/auth';
 
 
 export const startLoginEmailPassword = (email, password) => {
@@ -10,8 +10,26 @@ export const startLoginEmailPassword = (email, password) => {
         }, 3500)
     }
 }
-export const startGoogleLogin = () => {
+
+export const startRegisterEmailPassword = (name, email, password) => {
+
     return (dispatch) => {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(async ({ user }) => {
+                await updateProfile(user, { displayName: name })
+
+                dispatch(login(user.uid, user.displayName))
+            }).catch(e => {
+                console.log(e)
+            })
+    }
+}
+
+export const startGoogleLogin = () => {
+
+    return (dispatch) => {
+
         const auth = getAuth()
         signInWithPopup(auth, googleAuthProvider)
             .then(({ user }) => {
--- 08-journal-app/src/components/auth/RegisterScreen.js
+++ 08-journal-app/src/components/auth/RegisterScreen.js
@@ -3,6 +3,7 @@ import { useForm } from './../../hooks/useForm';
 import validator from 'validator';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeError, setError } from '../../actions/ui';
+import { startRegisterEmailPassword } from '../../actions/auth';
 
 export const RegisterScreen = () => {
 
@@ -22,6 +23,7 @@ export const RegisterScreen = () => {
         e.preventDefault();
 
         if (isFormValid()) {
+            dispatch(startRegisterEmailPassword(name, email, password));
         }
     }
 
