948914cbe923b5f63f07b0436dc78c109b62cd64
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-18 00:12:23 -0500

    SweetAlert 2 added and working

--- 08-journal-app/src/actions/auth.js
+++ 08-journal-app/src/actions/auth.js
@@ -2,18 +2,20 @@ import { googleAuthProvider } from '../firebase/firebase-config';
 import { types } from './../types/types';
 import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup, updateProfile } from 'firebase/auth';
 import { finishLoading, startLoading } from './ui';
-
+import Swal from 'sweetalert2';
+import { authErrors } from './../firebase/firebase-config';
 
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
         dispatch(startLoading())
-        signInWithEmailAndPassword(getAuth(), email, password)
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
             .then(({ user }) => {
                 dispatch(login(user.uid, user.displayName));
                 dispatch(finishLoading())
             }).catch(e => {
-                console.log(e)
                 dispatch(finishLoading())
+                Swal.fire('Error', authErrors[e.code], 'error')
             })
     }
 }
@@ -28,7 +30,7 @@ export const startRegisterEmailPassword = (name, email, password) => {
 
                 dispatch(login(user.uid, user.displayName))
             }).catch(e => {
-                console.log(e)
+                Swal.fire('Error', authErrors[e.code], 'error')
             })
     }
 }
@@ -41,6 +43,8 @@ export const startGoogleLogin = () => {
         signInWithPopup(auth, googleAuthProvider)
             .then(({ user }) => {
                 dispatch(login(user.uid, user.displayName))
+            }).catch(e => {
+                Swal.fire('Error', authErrors[e.code], 'error')
             })
     }
 
