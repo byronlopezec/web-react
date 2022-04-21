181b4a24703d6d3f1e36c6bbe878b972bd326672
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-21 00:14:23 -0500

    Style event

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -3,8 +3,11 @@ import React from 'react'
 import { Navbar } from './../ui/Navbar';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import 'moment/locale/es'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { calculateNewValue } from '@testing-library/user-event/dist/utils';
+import { messages } from './../../helper/calendar-message';
+
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
@@ -13,10 +16,24 @@ const events = [{
     start: moment().toDate(),
     end: moment().add(2, 'hours').toDate(),
     bgcolor: '#fafafa',
-}]
-
+}];
 
 export const CalendarScreen = () => {
+
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        console.log(event);
+
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white',
+        }
+        return {
+            style
+        }
+    }
+
     return (
         <div className='calendar-screen'>
             <Navbar />
@@ -26,6 +43,8 @@ export const CalendarScreen = () => {
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
             />
 
         </div>
