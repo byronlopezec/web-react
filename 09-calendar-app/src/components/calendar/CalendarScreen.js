8071d5a35ec3a08a7b3bf8900e0f301c74f55508
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-20 23:42:06 -0500

    React Big Calendar added

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,11 +1,33 @@
 
 import React from 'react'
 import { Navbar } from './../ui/Navbar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { calculateNewValue } from '@testing-library/user-event/dist/utils';
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title: 'All Day Event very long title',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa',
+}]
+
 
 export const CalendarScreen = () => {
     return (
-        <div>
+        <div className='calendar-screen'>
             <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+            />
+
         </div>
     )
 }
--- 09-calendar-app/src/index.js
+++ 09-calendar-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CalendarApp } from './CalendarApp';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<CalendarApp />);
