48c3ce1689ba422ffed3446e6710b2a089b2f323
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-25 23:20:11 -0500

    Recuperar información post

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -2,17 +2,32 @@ const { response } = require('express');
 
 
 const crearUsuario = (req, res = response) => {
+
+    const { name, email, password } = req.body;
+
+    if(name.length <= 5) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'El nombre debe ser mayor a 5 caracteres'
+        });
+    }
+
     res.json({
         ok: true,
-        msg: 'crearUsuario'
+        name,
+        email,
+        password
     });
 }
 
 
 const loginUsuario = (req, res = response) => {
+    const { email, password } = req.body;
+
     res.json({
         ok: true,
-        msg: 'loginUsuario'
+        email,
+        password
     });
 }
 
--- 10-calendar-backend/index.js
+++ 10-calendar-backend/index.js
@@ -9,6 +9,9 @@ const app = express();
 // Directorio publico
 app.use(express.static('public'));
 
+// Lectura y parseo del body
+app.use(express.json());
+
 // Rutas
 app.use('/api/auth',require('./routes/auth'));
 
