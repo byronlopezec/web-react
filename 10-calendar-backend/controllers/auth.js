b1557056d0cf266bc93e4221f526d85e02b4d226
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-25 23:32:21 -0500

    express validator

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,18 +1,29 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 
 const crearUsuario = (req, res = response) => {
 
     const { name, email, password } = req.body;
 
-    if(name.length <= 5) {
+    // if(name.length <= 5) {
+    //     return res.status(400).json({
+    //         ok: false,
+    //         msg: 'El nombre debe ser mayor a 5 caracteres'
+    //     });
+    // }
+
+    // manejo de errores
+    const errores = validationResult(req);
+
+    if (!errores.isEmpty()) {
         return res.status(400).json({
             ok: false,
-            msg: 'El nombre debe ser mayor a 5 caracteres'
+            errores: errores.mapped()
         });
     }
 
-    res.json({
+    res.status(201).json({
         ok: true,
         name,
         email,
@@ -24,7 +35,18 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
     const { email, password } = req.body;
 
-    res.json({
+    // manejo de errores
+    const errores = validationResult(req);
+
+    if (!errores.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errores: errores.mapped()
+        });
+    }
+
+
+    res.status(200).json({
         ok: true,
         email,
         password
--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -8,15 +8,36 @@
 // const router = express.Router();
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { crearUsuario, revalidarToken, loginUsuario } = require('../controllers/auth');
 
-router.post('/new', crearUsuario);
+router.post(
+    '/new',
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'La contraseña es obligatoria').not().isEmpty(),
+        check('password', 'La contraseña debe ser mayor a 5 caracteres').isLength({ min: 6 })
+    ],
+    crearUsuario
+);
 
-router.post('/', loginUsuario);
+router.post(
+    '/',[
+        check('email', 'El email es obligatorio').not().isEmpty(),
+        check('email', 'El email es incorrecto').isEmail(),
+        check('password', 'La contraseña es obligatoria').not().isEmpty(),
+        check('password', 'La contraseña debe ser mayor a 5 caracteres').isLength({ min: 6 })
+    ],
+    loginUsuario
+);
 
-router.get('/renew', revalidarToken)
+router.get(
+    '/renew',
+    revalidarToken
+)
 
 
 
