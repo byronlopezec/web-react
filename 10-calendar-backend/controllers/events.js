4c7456ff1a0ee43fda9eb1f5528e9cb2f3033039
buhobyte <byron.lopez.begins@gmail.com>
2022-04-27 13:36:19 -0500

    Update Event

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -35,12 +35,49 @@ const crearEvento = async (req, res = response) => {
 
 }
 
-const actualizarEvento = (req, res = response) => {
+const actualizarEvento = async(req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: "Evento actualizado"
-    });
+    const eventoId = req.params.id;
+
+    try {
+        const evento = await Evento.findById(eventoId);
+
+        if (!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El evento no existe'
+            });
+        }
+
+        if (evento.user.toString() !== req.uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tienes permiso para actualizar este evento'
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: req.uid
+        }
+
+        // new = true permite que devuelva el objeto actualizado
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento,{new: true});
+
+        res.json({
+            ok: true,
+            eventoActualizado
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado',
+            error
+        });
+    }
+
 }
 
 const eliminarEvento = (req, res = response) => {
--- 10-calendar-backend/routes/events.route.js
+++ 10-calendar-backend/routes/events.route.js
@@ -25,7 +25,14 @@ router.post("/",
     crearEvento);
 
 // Actualizar Eventos
-router.put("/:id", actualizarEvento);
+router.put("/:id",
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'La fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'La fecha de fin es obligatoria').custom(isDate),
+        validarCampos
+    ],
+    actualizarEvento);
 
 // Eliminar Eventos
 router.delete("/:id", eliminarEvento);
