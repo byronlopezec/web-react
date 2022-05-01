cc1a50423bbc557d0817dcf8dc60b385e8a37251
Buhobyte <byron.lopez.begins@gmail.com>
2022-05-01 13:08:07 -0500

    DeleteEventFab

---
+++ 09-calendar-app/src/tests/components/ui/DeleteEventFab.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import '@testing-library/jest-dom'
+import { DeleteDeventFab } from '../../../components/ui/DeleteDeventFab';
+import { eventStartDelete } from './../../../components/actions/events';
+
+const mockStore = configureStore([thunk]);
+
+const initialState = {}
+const store = mockStore(initialState);
+store.dispatch = jest.fn();
+
+const wrapper = mount(
+    <Provider store={store}>
+        <DeleteDeventFab />
+    </Provider>
+)
+
+jest.mock('./../../../components/actions/events', () => ({ 
+    eventStartDelete: jest.fn()
+ }))
+
+
+describe('Pruebas en <DeleteEventFav />.', () => {
+
+
+    test('debe de motrarse correctamente', () => {
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe llamar el eventStartdelete al hacer click', () => {
+
+  
+        wrapper.find('button').prop('onClick')();
+        
+        expect(eventStartDelete).toHaveBeenCalled();
+
+    });
+});
