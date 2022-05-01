a14be533e43e7660063071505edd49ce9dd8e1c1
Buhobyte <byron.lopez.begins@gmail.com>
2022-04-30 22:38:30 -0500

    fetchSin token test

--- 09-calendar-app/src/tests/helpers/fetch.test.js
+++ 09-calendar-app/src/tests/helpers/fetch.test.js
@@ -1,8 +1,10 @@
-import { fetchSinToken } from "../../helper/fetch";
+import { fetchConToken, fetchSinToken } from "../../helper/fetch";
 
 
 
 describe("Pruebas en Helper", () => {
+
+    let token;
 
     test('fetchSinToken debe funcionar', async () => {
         const resp = await fetchSinToken('auth', { email: "byron.lopez.begins@gmail.com", password: "123456" }, 'POST');
@@ -10,6 +12,18 @@ describe("Pruebas en Helper", () => {
 
         const body = await resp.json();
         expect(body.ok).toBe(true);
+        token = body.token;
+
+    })
+
+    test('fetchSinToken save token', async () => {
+
+        localStorage.setItem('token', token);
+
+        const resp = await fetchConToken('events/6269856d4c71763860849c42',{}, 'DELETE');
+        const body = await resp.json();
+
+        expect(body.msg).toBe('El evento no existe');
 
     })
 
